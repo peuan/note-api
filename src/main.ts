7729ea02a138e8bf8413574c6cef797bbf202c4e
niawjunior <niawjunior@niawjuniors-MacBook-Pro.local>
2020-11-21 15:29:07 +0700

    create method add note

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
 import { Swagger } from './common/swagger';
 import { AppLogger } from './logger/logger.service';
 
-
 async function bootstrap() {
-
-  const app = await NestFactory.create(AppModule,{
-    logger: false
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
   });
-  app.use(RequestIdMiddleware)
-  app.useLogger(new AppLogger())
+  app.use(RequestIdMiddleware);
+  app.useLogger(new AppLogger());
   app.enableCors();
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   new Swagger(app);
 
   await app.listen(5000);
--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class LoginDto {
   @IsString()
+  @ApiProperty({ example: 'note' })
   username: string;
 
+  @ApiProperty({ example: 'noteP@ssword' })
   @IsString()
   password: string;
 }

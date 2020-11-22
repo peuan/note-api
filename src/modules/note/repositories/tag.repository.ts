cfec21c00aa00ebd13706a133aa581cf383574f4
niawjunior <niawjunior@niawjuniors-MacBook-Pro.local>
2020-11-22 13:18:21 +0700

    create tag feature

---
+++ src/modules/note/repositories/tag.repository.ts
@@ -0,0 +1,12 @@
+import { User } from 'src/modules/auth/entities/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { Tag } from '../entities/tag.entity';
+
+@EntityRepository(Tag)
+export class TagRepository extends Repository<Tag> {
+  async findOneTagByUser(user: User, tag: string) {
+    return await this.findOne({
+      where: { user, tag },
+    });
+  }
+}
